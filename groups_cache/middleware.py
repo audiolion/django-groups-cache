8c30350f510c7810ee2ecabc35e2ed325d49bcb2
Ryan Castner <castner.rr@gmail.com>
2017-01-11 16:53:48 -0500

    Removed MiddlewareMixin due to deprecation and incompatibility with Django 1.8

--- groups_cache/middleware.py
+++ groups_cache/middleware.py
@@ -1,11 +1,24 @@
 from django.conf import settings
 from django.http import HttpResponse
-from django.utils.deprecation import MiddlewareMixin
 
 from .utils import get_group
 
 
-class GroupsMiddleware(MiddlewareMixin):
+class GroupsCacheMiddleware(object):
+
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+        super(MiddlewareMixin, self).__init__()
+
+    def __call__(self, request):
+        response = None
+        if hasattr(self, 'process_request'):
+            response = self.process_request(request)
+        if not response:
+            response = self.get_response(request)
+        if hasattr(self, 'process_response'):
+            response = self.process_response(request, response)
+        return response
 
     @property
     def lazy_groups(self):
