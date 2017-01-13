7cd710691b4b3a9def5d756f8fe2e5ba4fca897a
Ryan Castner <castner.rr@gmail.com>
2017-01-12 21:58:27 -0500

    Add sessions so we can simulate requests with the client

--- runtests.py
+++ runtests.py
@@ -17,6 +17,7 @@ try:
             "django.contrib.auth",
             "django.contrib.contenttypes",
             "django.contrib.sites",
+            "django.contrib.sessions",
             "groups_cache",
         ],
         SITE_ID=1,
