da12b2159b02568b334b9c731fa974bea5135475
Ryan Castner <castner.rr@gmail.com>
2017-01-12 21:58:04 -0500

    Add missing signal on m2m groups ManyToMany field changing and not causing the related user(s) cache to invalidate

--- groups_cache/signals.py
+++ groups_cache/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 from django.conf import settings
 from django.contrib.auth.models import Group
 from django.core.cache import cache
@@ -18,3 +18,9 @@ def invalidate_group_cache(sender, **kwargs):
     for user in users:
         key = generate_cache_key(user)
         cache.delete(key)
+
+def invalidate_user_m2m_cache(sender, **kwargs):
+    key = generate_cache_key(kwargs.get('instance'))
+    cache.delete(key)
+
+m2m_changed.connect(invalidate_user_m2m_cache, sender=Group.user_set.through)
